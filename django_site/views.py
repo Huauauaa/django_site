426d040f526828e9d3f84804ff04f06ab05a7fec
huaweiwei <harvey0379@163.com>
2021-09-16 13:40:03 +0800

    feat: CBV csrf_exempt

--- django_site/views.py
+++ django_site/views.py
@@ -1,16 +1,19 @@
 from django.http import HttpResponse
 from django.views import View
 import json
-
+from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 
 
 class StudentView(View):
     students = ['John Doe', 'Micheal']
 
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
     def get(self, request):
         return HttpResponse(json.dumps(self.students))
 
-    @csrf_exempt
     def post(self, request):
         return HttpResponse('POST')
