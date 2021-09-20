bd3b4490f38d9ee49c8626a62f3ad04d35307991
Hua Weiwei <15339206608@163.com>
2021-09-20 20:43:48 +0800

    add requests demo

---
+++ tests/test.py
@@ -0,0 +1,18 @@
+import requests, json, yaml
+
+with open('config.local.yaml', 'r') as ymlfile:
+    config = yaml.load(ymlfile)
+
+domain = 'http://8.142.54.87:1880'
+
+r = requests.post(f'{domain}/c/login',
+                  data={
+                      'principal': config['harbor']['username'],
+                      'password': config['harbor']['password']
+                  })
+
+r1 = requests.get(f'{domain}/api/users/current', cookies=r.cookies)
+
+result = json.loads(r1.text)
+
+print(result)
