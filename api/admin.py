b6716c853d80f841580fe286257332c3366b7346
Hua Weiwei <15339206608@163.com>
2021-09-28 21:38:09 +0800

    many to many

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,6 @@
+from api.models import Publication
 from django.contrib import admin
 
-from .models import Student, Book, Copyright
+from .models import Student, Book, Copyright, Publication, Author
 
-admin.site.register([Student, Book, Copyright])
+admin.site.register([Student, Book, Copyright, Publication, Author])
--- api/models/Book.py
+++ api/models/Book.py
@@ -1,3 +1,4 @@
+from api.models.Author import Author
 from api.models.Publication import Publication
 from api.models.Copyright import Copyright
 from django.db import models
@@ -7,6 +8,7 @@ class Book(models.Model):
     name = models.CharField('书名', max_length=20)
     publication = models.ForeignKey(Publication, on_delete=models.CASCADE)
     copyright = models.OneToOneField(Copyright, on_delete=models.CASCADE)
+    authors = models.ManyToManyField(Author)
 
     def __str__(self) -> str:
         return f'[Book]-{self.name}'
